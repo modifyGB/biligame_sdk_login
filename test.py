054403590037a84159d20b9edc7b91b3e87f4dab
modifyGB <2782299413@qq.com>
2021-08-08 20:04:59 +0800

    commit

--- test.py
+++ test.py
@@ -12,50 +12,62 @@ header = {
     "Host": "line1-sdk-center-login-sh.biligame.net",
     "Connection": "Keep-Alive",
 }
-form = {
-    "operators": "1",
+form1 = {
     "merchant_id": "328",
-    "isRoot": "1",
-    "domain_switch_count": "0",
-    "sdk_type": "1",
-    "sdk_log_type": "1",
     "timestamp": "1628330079265",
-    "support_abis": "x86,armeabi-v7a,armeabi",
-    "access_key": "", # 登录key
-    "sdk_ver": "3.10.4",
-    "oaid": "",
-    "dp": "",
-    "original_domain": "",
-    "imei": "540000000140886",
     "version": "1",
-    "udid": "KREhESMUdUV0RyIXaxdrEWlYPws5WipAJA==",
-    "apk_sign": "4502a02a00395dec05a4134ad593224d",
-    "platform_type": "3",
-    "old_buvid": "XZ7F9B979B64C22B1F124F89C659252A8A09B",
-    "android_id": "dc428b9ba0b6b4d1",
-    "fingerprint": "",
-    "mac": "08:00:27:A0:13:E5",
     "server_id": "1178",
-    "domain": "line1-sdk-center-login-sh.biligame.net",
-    "app_id": "952",
-    "version_code": "29",
-    "net": "4",
-    "pf_ver": "6.0.1",
-    "cur_buvid": "XZ7F9B979B64C22B1F124F89C659252A8A09B",
-    "c": "1",
-    "brand": "HUAWEI",
-    "client_timestamp": "1628330078528",
-    "channel_id": "1",
-    "uid": "",
     "game_id": "952",
-    "ver": "1.5.60",
-    "model": "ALP-AL00",
+
     # "sign": "dcfc2709d88f0c9dff2eb3bfed114340", # 签名
     # "pwd": "", # 加密密码
     # "user_id": "" # 账号
+
+    # "operators": "1",
+    # "isRoot": "1",
+    # "domain_switch_count": "0",
+    # "sdk_type": "1",
+    # "sdk_log_type": "1",
+    # "support_abis": "x86,armeabi-v7a,armeabi",
+    # "access_key": "",
+    # "sdk_ver": "3.10.4",
+    # "oaid": "",
+    # "dp": "",
+    # "original_domain": "",
+    # "imei": "540000000140886",
+    # "udid": "KREhESMUdUV0RyIXaxdrEWlYPws5WipAJA==",
+    # "apk_sign": "4502a02a00395dec05a4134ad593224d",
+    # "platform_type": "3",
+    # "old_buvid": "XZ7F9B979B64C22B1F124F89C659252A8A09B",
+    # "android_id": "dc428b9ba0b6b4d1",
+    # "fingerprint": "",
+    # "mac": "08:00:27:A0:13:E5",
+    # "domain": "line1-sdk-center-login-sh.biligame.net",
+    # "app_id": "952",
+    # "version_code": "29",
+    # "net": "4",
+    # "pf_ver": "6.0.1",
+    # "cur_buvid": "XZ7F9B979B64C22B1F124F89C659252A8A09B",
+    # "c": "1",
+    # "brand": "HUAWEI",
+    # "client_timestamp": "1628330078528",
+    # "channel_id": "1",
+    # "uid": "",
+    # "ver": "1.5.60",
+    # "model": "ALP-AL00",
 }
-url_rsa = "https://line1-sdk-center-login-sh.biligame.net/api/client/rsa"
-url_login = "https://line1-sdk-center-login-sh.biligame.net/api/client/login"
+form3 = {
+    "merchant_id": "328",
+    "timestamp": "1628330079265",
+    "version": "3",
+    "server_id": "1178",
+    "game_id": "952",
+    "cipher_type": "bili_login_rsa",
+}
+url_rsa1 = "https://line1-sdk-center-login-sh.biligame.net/api/client/rsa"
+url_rsa3 = "https://line1-sdk-center-login-sh.biligame.net/api/external/issue/cipher/v3"
+url_login1 = "https://line1-sdk-center-login-sh.biligame.net/api/client/login"
+url_login3 = "https://line1-sdk-center-login-sh.biligame.net/api/external/login/v3"
 
 user_id = ""
 password = ""
@@ -76,10 +88,10 @@ def format_form(form_):
     return "&".join(["{}={}".format(i, urllib.parse.quote(form_[i])) for i in form_.keys()])
 
 def main():
-    js = json.loads(requests.post(url_rsa,headers=header, data=get_rsa_form(form)).text)
-    form_ = copy.deepcopy(form)
+    js = json.loads(requests.post(url_rsa1,headers=header, data=get_rsa_form(form1)).text)
+    form_ = copy.deepcopy(form1)
     form_['user_id'] = user_id
-    print(requests.post(url_login,headers=header, data=get_login_form(form_, js['hash'] + password)).text)
+    print(requests.post(url_login1,headers=header, data=get_login_form(form_, js['hash'] + password)).text)
 
 if __name__ == "__main__":
     main()
